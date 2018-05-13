37da52e9c42f89d07aa00d92a3bf5eb42282abcf
pyxarez <pyxarez@gmail.com>
2018-05-13 16:43:30 +0300

    Add basic project pages and components

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { CreatePostComponent } from './create-post/create-post.component';
+import { PostPageComponent } from './post-page/post-page.component';
+import { MainComponent } from './main/main.component';
+
+const routes: Routes = [
+  { path: '', component: MainComponent },
+  { path: 'create', component: CreatePostComponent },
+  { path: 'post/:id', component: PostPageComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -1,5 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { AngularSvgIconModule } from 'angular-svg-icon';
 
 import { AppRoutingModule } from './app-routing.module';
 import { QuillModule } from 'ngx-quill';
@@ -7,6 +10,11 @@ import { QuillModule } from 'ngx-quill';
 import { AppComponent } from './app.component';
 
 import Quill from 'quill';
+import { HeaderComponent } from './header/header.component';
+import { EditorComponent } from './editor/editor.component';
+import { CreatePostComponent } from './create-post/create-post.component';
+import { MainComponent } from './main/main.component';
+import { PostPageComponent } from './post-page/post-page.component';
 
 window.Quill = Quill;
 
@@ -16,12 +24,20 @@ window.Quill.register('module/imageResize', ImageResize);
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    EditorComponent,
+    CreatePostComponent,
+    MainComponent,
+    PostPageComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    QuillModule
+    QuillModule,
+    FormsModule,
+    HttpClientModule,
+    AngularSvgIconModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
