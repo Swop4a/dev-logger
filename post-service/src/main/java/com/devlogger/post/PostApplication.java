c5b5c26fb68b598f7afc6052f2930e23cc0693ac
Alexandr <alexandr300896@gmail.com>
2018-05-27 16:46:38 +0300

    Commit after merge

--- post-service/src/main/java/com/devlogger/post/PostApplication.java
+++ post-service/src/main/java/com/devlogger/post/PostApplication.java
@@ -3,7 +3,6 @@ package com.devlogger.post;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.feign.EnableFeignClients;
 
 /**
  * @author swop4a
