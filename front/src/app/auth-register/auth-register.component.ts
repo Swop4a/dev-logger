42005e072ee6cf84eedbec75aeee63a6b88cede8
pyxarez <pyxarez@gmail.com>
2018-05-27 08:23:43 +0300

    Add validations

--- front/src/app/auth-register/auth-register.component.ts
+++ front/src/app/auth-register/auth-register.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
 import { Store } from '@ngrx/store';
 
+import { PasswordValidation } from '../password-validation';
+
 import {
   LOGIN,
   REGISTER_USER,
@@ -39,7 +41,7 @@ export class AuthRegisterComponent implements OnInit {
     this.registerForm = this.fb.group({
       username: ['', Validators.required],
       password: ['', Validators.required],
-      repeatPassword: ['', Validators.required],
+      confirmPassword: ['', Validators.required, PasswordValidation.MatchPassword],
     });
   }
 
